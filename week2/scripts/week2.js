ec8b4d9bec3e34a5787c9b9330edd07c899a69dd
woonohyo <woonohyo@nhnnext.org>
2014-07-18 20:08:03 +0900

    comment added

--- week2/scripts/week2.js
+++ week2/scripts/week2.js
@@ -1,8 +1,10 @@
+// {div의 ID:출력할 문구}
 var yasikDict = {"chickenAndBeer":"치맥은", "pajeonAndMakgeolli":"파전에 막걸리는", "tripeAndSoju":"곱창에 쏘주는"};
 
 var myObj = {
 	strTexts: " 정말 몸에 좋을까..요?",
 	registerEvents: function(event) {
+		// 마우스 클릭 이벤트가 일어난 Element의 ID를 통해 사용자가 누른 사진이 무엇인지 판별합니다.
 		var yasikName = event.target.parentNode.id;
 		console.log(yasikDict[yasikName] + this.strTexts);
 	}
@@ -10,6 +12,8 @@ var myObj = {
 
 var yasik = document.querySelectorAll(".wrapper div");
 
+// bind를 통해 registerEvents 함수의 this는 myObj가 됩니다.
+// 만약 bind를 하지 않으면 registerEvents의 this == window가 되기 때문에 this.strTexts에 접근을 못하기 때문입니다.
 for (var i = 0; i < yasik.length; i++) { 
 	yasik[i].addEventListener("click", myObj.registerEvents.bind(myObj), false);
 }
